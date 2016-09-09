62f7692c84acdfc5c93360dd2bac8d36846118cb
Ivan Radchenko <radchenk@gmail.com>
2016-09-09 11:50:45 +0300

    Add hover jquery and checkbox check with Courses item fadeout

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,19 +1,24 @@
-// $( "#js" ).click
-// 	(function() {
-// 	$( 'courses__item' ).fadeOut( '500' );
-// });
-
+  /*_____ Add hover class _____ */
 
 $( ".courses__item" ).hover(
 	function(e) {
 		$( this ).addClass( "hover" );
 	}, function() {
 		$( this ).removeClass( "hover" );
-		// body...
 	}
 );
 
 
 
-  /*_____ Completed Overlay_____ */
-  
+  /*_____ Clickable Courses Item in order to fadeout and check the checkbox _____ */
+  $ ( ".courses__item" ).click( function () {
+    $(this).find(".checkbox").prop('checked', true);
+    $( event.target ).closest( ".courses__item" ).fadeOut("400");
+ });
+
+
+/*_____ Overlay Complete Statement if all lis displayed none____ */
+var ulempy = $('ul:empty');
+if ulempy {
+  alert("hey");
+}
